d99afbbb8bb462f63610dcabc8a184cd443cb223
Luis Leonardo Ascanio <leonardoa123@gmail.com>
2024-05-23 17:17:49 -0400

    resolve vite.config.js 2

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: './index.html',  // Asegúrate de que esto apunte al archivo correcto
-    },
+    outDir: 'dist',  // Asegúrate de que esto apunte al directorio correcto
   },
-});
+})
